5f0afaed4f5ab59ab9710eb6ff11b608a6212533
Peauli Ghosh <peauli231001102272@technoindiaeducation.com>
2026-03-19 20:45:59 +0530

    Task 02 Step 3: Created User model

---
+++ backend/app/models/user_model.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, String, Integer
+from app.db.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(String, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    age = Column(Integer, nullable=False)
